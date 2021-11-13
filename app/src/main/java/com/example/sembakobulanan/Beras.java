818008a58e3aeb806a77e44f7ad868daaddbebcf
dhevz <66546628+dhevz@users.noreply.github.com>
2021-11-13 17:30:57 +0700

    first commit

---
+++ app/src/main/java/com/example/sembakobulanan/Beras.java
@@ -0,0 +1,14 @@
+package com.example.sembakobulanan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Beras extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beras);
+    }
+}
---
+++ app/src/main/java/com/example/sembakobulanan/Gula.java
@@ -0,0 +1,14 @@
+package com.example.sembakobulanan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Gula extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gula);
+    }
+}
